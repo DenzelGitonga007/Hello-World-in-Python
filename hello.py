dd49767d8e39f5947620a7ac30ccf7e1680f0807
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-03-10 23:22:47 +0300

    crusade day

--- hello.py
+++ hello.py
@@ -3,3 +3,4 @@ msg = "Hello World, Denzel is learning Python"
 print(msg)
 
 print("{} has been a tiresome day".format(date.today())) # 2023-03-08
+print("{} has also been a tiresome day".format(date.today())) # 2023-03-10
